8a1481d70b4b2ff0a4de22fdd6f3fe36d13f42d6
Zulfiqar Tariq <zulfiqar_tariq@hotmail.com>
2019-09-21 18:05:48 +0500

    remove confilict in task compnent

--- resources/js/components/TaskComponent.vue
+++ resources/js/components/TaskComponent.vue
@@ -1,37 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">All todo Tasks</div>
-
-                    <div class="card-body">
-
-                        <button type="button"
-                                class="btn btn-primary"
-                                data-toggle="modal"
-                                data-target="#myModal">
-                            Open modal
-                        </button>
-
-                        <ul class="list-group" v-for=" t in tasks">
-                            <li>
-                                {{t.id}} - {{t.name}}
-                                <button type="button"
-                                        class="btn btn-primary"
-                                        data-toggle="modal"
-                                        data-target="#editModal">
-                                    Edit
-                                </button>
-                            </li>
-                        </ul>
-
-                    </div>
-                </div>
-            </div>
-
-=======
     <div class="container" style="border:solid 2px yellow;">
         <div class="col-md-8">
             <ul class="list-group" v-for="task in tasks">
@@ -43,54 +10,21 @@
             {{tasks}}
         </div>
         <input type="text" v-model="test"/>
->>>>>>> e72058590f80e413a7624d2ac5ae510483a0e0cc
 
         <div>
             {{errors}}
         </div>
-<<<<<<< HEAD
-        <addtask @recordAdded="refreshRecord"></addtask>
-        <edittask :rec="editRec"></edittask>
-        <sample-component v-bind:inputvalues="heybaby"></sample-component>
-=======
 
 
         <samplecomponent v-bind:tasks="tasks" v-bind:inputvalue="test"></samplecomponent>
->>>>>>> e72058590f80e413a7624d2ac5ae510483a0e0cc
     </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-
-    import SampleComponent from "./SampleComponent";
-    Vue.component('addtask', require('./addModelComponent.vue').default);
-    Vue.component('edittask', require('./EditModelComponent.vue').default);
-=======
     Vue.component('samplecomponent', require('./SampleComponent.vue').default);
->>>>>>> e72058590f80e413a7624d2ac5ae510483a0e0cc
     export default {
-        components: {SampleComponent},
         data() {
             return {
-<<<<<<< HEAD
-                heybaby:"why are you fucing doing this asd",
-                tasks: {},
-                editRec:{},
-                errors:[],
-
-
-            }
-        },
-        methods: {
-            refreshRecord(record) {
-                this.tasks.unshift(record.data)
-            },
-            getRecordId(id){
-                // axios.get('http://tundor.test/tasks'+id)
-                //     .then((response) => this.tasks = response.data)
-                //     .catch( error => this.errors= error.response.data.errors);
-=======
                 test:"",
                 tasks: [],
                 update: {
@@ -114,7 +48,6 @@
                     .then(function (response) {
                         console.log(response.data);
                     })
->>>>>>> e72058590f80e413a7624d2ac5ae510483a0e0cc
             }
         },
         created() {
