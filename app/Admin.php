0e5ddd74a5c1acdb5a6aa85c47fb6ca1e26e178b
Your Name <zulfqiar_tariq@hotmail.com>
2019-10-04 15:05:17 +0500

    hi

--- app/Admin.php
+++ app/Admin.php
@@ -25,7 +25,7 @@ class Admin extends Authenticatable implements JWTSubject
      *
      * @var array
      */
-    protected  $hidden = [
+    protected   $hidden = [
         'password', 'remember_token',
     ];
 
